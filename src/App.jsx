447006cfb1f9d399f0e31f4122b37b734125d176
Kamil Sobik <kamil.sobik@gmail.com>
2023-01-31 21:40:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <React.Fragment>
+      <h1>Warehouse Management System</h1>
+      <h2>working in progress...</h2>
+    </React.Fragment>
+  );
+}
+
+export default App;
